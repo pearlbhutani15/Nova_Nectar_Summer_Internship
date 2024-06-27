f06b3f5bb05b453d83ddfb3906c9ec3d7d52a30a
pearlbhutani15 <pearlbhutani1819@gmail.com>
2024-06-27 18:06:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+js:
+// Mobile Menu Toggle
+const menuToggle = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+});
